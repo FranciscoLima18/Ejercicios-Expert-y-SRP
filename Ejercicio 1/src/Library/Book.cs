627faf8213af77734f7f0eff607ea3954e92bced
Francisco lima <francisco.lima@correo.ucu.edu.uy>
2023-09-03 21:23:35 -0300

    Cambios del ejercicio 1 (library y program).

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -4,25 +4,30 @@ namespace Ucu.Poo.Expert
 {
     public class Book
     {
+        public string Title { get; }
+        public string Author { get; }
+        public string Code { get; }
 
-        public string Title { get ; }
-        public string Author { get ; }
-        public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
-
-        public Book(String title, String author, String code)
+        public Book(string title, string author, string code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
         }
+    }
 
-        public void ShelveBook(String sector, String shelve)
+    public class LibraryBook
+    {
+        public string LibrarySector { get; set; }
+        public string LibraryShelve { get; set; }
+
+        public static void ShelveBook(string sector, string shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
-
     }
 }
+
+/* La clase BOOK NO cumple con el principio SRP, ya que las responsabilidades asignadas deberían estar divididas en 2 clases diferentes (public Book, public void ShelveBook)/*
+
